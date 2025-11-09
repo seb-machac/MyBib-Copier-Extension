c2dc2eb60c7a5e29dd168a782f3c670d550dcc38
Sebma <147602507+seb-machac@users.noreply.github.com>
2025-11-10 00:36:56 +1100

    Created Repo

---
+++ Source/background.js
@@ -0,0 +1,23 @@
+﻿chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"]
+  });
+});
+
+chrome.runtime.onMessage.addListener((msg, sender) => {
+  if (msg === "FINISHED") {
+    chrome.action.setIcon({ path: "IconCopied.png", tabId: sender.tab.id });
+
+    setTimeout(() => {
+      chrome.action.setIcon({ path: "Icon.png", tabId: sender.tab.id });
+    }, 2000);
+  }
+  else if (msg === "ERROR") {
+    chrome.action.setIcon({ path: "IconFailed.png", tabId: sender.tab.id });
+
+    setTimeout(() => {
+      chrome.action.setIcon({ path: "Icon.png", tabId: sender.tab.id });
+    }, 2000);
+  }
+});
---
+++ Source/content.js
@@ -0,0 +1,31 @@
+﻿(async () => {
+  const spans = document.querySelectorAll("div[style*='padding: 0px 1in'] > span:first-child");
+
+  const items = [...spans].map(span => {
+    const html = span.innerHTML;
+    const titleMatch = html.match(/<i>(.*?)<\/i>/);
+    const title = titleMatch ? titleMatch[1].trim() : null;
+    const urlMatch = span.innerText.match(/https?:\/\/\S+/);
+    const url = urlMatch ? urlMatch[0] : null;
+    return title && url ? { title, url } : null;
+  }).filter(x => x);
+
+  if (!items.length) {
+    chrome.runtime.sendMessage("ERROR");
+    alert("No citations found.");
+    return;
+  }
+
+  const htmlLinks = items.map(i => `<a href="${i.url}">${i.title}</a>`).join("<br>");
+  const plainText = items.map(i => `${i.title} - ${i.url}`).join("\n");
+
+  const data = [
+    new ClipboardItem({
+      "text/html": new Blob([htmlLinks], { type: "text/html" }),
+      "text/plain": new Blob([plainText], { type: "text/plain" })
+    })
+  ];
+
+  await navigator.clipboard.write(data);
+  chrome.runtime.sendMessage("FINISHED");
+})();
